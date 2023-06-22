698ab5d5c6b4e9f90a8862a00f863eac919268ec
Allen Kim <avalmercafe02@gmail.com>
2023-06-21 23:07:02 -0400

    test

---
+++ client/src/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+function NavBar () {
+
+    return (
+        <h1> This is a nav bar</h1>
+    )
+}
+
+export default NavBar 
---
+++ client/src/SignUpPage.js
@@ -0,0 +1,8 @@
+import React from "react"
+function SignUpPage () {
+    return (
+        <h1> This is a sign up page</h1>
+    )
+}
+
+export default SignUpPage
