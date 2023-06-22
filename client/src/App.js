8a66c0538f1a2c887e5f7cb0150989d32be1b2d5
Allen Kim <avalmercafe02@gmail.com>
2023-06-21 23:29:18 -0400

    fixed error

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,12 @@ import {Routes,Route,BrowserRouter} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-    
+     <BrowserRouter>
      <NavBar/>
-     
- 
+     <Routes>
+        <Route path="/signUpForms" element={<SignUpPage/>}/>
+     </Routes>
+     </BrowserRouter>
 
     </div>
   );
