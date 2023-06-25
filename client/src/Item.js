27151340ba937b728d79085ca2bb048e87cf4bf9
Allen Kim <avalmercafe02@gmail.com>
2023-06-24 23:36:14 -0400

    item list/items

--- client/src/Item.js
+++ client/src/Item.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-function Item () {
+function Item ({category,price,condition,wearer,name,seller,photo,brand,size,index,id}) {
     return (
-        <div>
-
+        <>
+        <div className="item">
+          <img alt="itemLogo" src={id === 6 ? "/beanhut.jpg" : photo}/> 
+          <div className="itemInfo">
+            <h1>{`${index + 1}. ${name}`}</h1>
+            <p><b>Price:</b> ${price}</p>
+            <p><b>Condition:</b> {condition}</p>
+            <p><b>Category:</b> {category}</p>
+            <p><b>Size:</b> {size}</p>
+            <p><b>Brand:</b> {brand}</p>
+            <p><b>For</b> {wearer}</p>
+            
+          </div>
         </div>
+     
+    </>
     )
 }
 
