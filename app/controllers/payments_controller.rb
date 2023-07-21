96b4c9a26e5075e0d33bfc960d187698fbd78fc5
Allen Kim <avalmercafe02@gmail.com>
2023-07-21 16:52:49 -0400

    commit tree e35f847511d11975e4a8fe985753865cce8c0363

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,9 +2,9 @@ class PaymentsController < ApplicationController
     def create_payment_intent
       # Get the cart items and calculate the total amount (replace this with your logic)
       cart_items = params[:cart_items]
+      total_amount = calculate_total_amount(cart_items)
       total_amount_in_cents = (total_amount * 100).to_i
-      
-  
+      byebug
       # Create the PaymentIntent using the Stripe API
       begin
         intent = Stripe::PaymentIntent.create({
@@ -21,8 +21,8 @@ class PaymentsController < ApplicationController
     private
   
     # Replace this with your logic to calculate the total amount based on cart items
-    def calculate_total_amount(cart_items)
-      total = cart_items.sum { |item| item[:price] }
-      total
-    end
+   def calculate_total_amount(cart_items)
+  total = cart_items.sum { |item| item[:price] }
+  total
+end
   end
